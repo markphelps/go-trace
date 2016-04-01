c8762894cb4cb7a0e92d77a9876b2a7345509c13
Mark Phelps <mark.aaron.phelps@gmail.com>
2016-03-31 20:30:02 -0400

    comments and cleanup

--- main.go
+++ main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
-	p "github.com/markphelps/go-trace/primatives"
 	"math"
 	"math/rand"
 	"os"
+
+	p "github.com/markphelps/go-trace/primatives"
 )
 
 const (
-	nx = 400
-	ny = 200
-	ns = 100
+	nx = 400 // size of x
+	ny = 200 // size of y
+	ns = 100 // number of samples for aa
 	c  = 255.99
 )
 
@@ -21,8 +22,8 @@ var (
 
 	camera = p.NewCamera()
 
-	sphere = p.Sphere{Center: p.Vector{0, 0, -1}, Radius: 0.5}
-	floor  = p.Sphere{Center: p.Vector{0, -100.5, -1}, Radius: 100}
+	sphere = p.Sphere{p.Vector{0, 0, -1}, 0.5}
+	floor  = p.Sphere{p.Vector{0, -100.5, -1}, 100}
 
 	world = p.World{[]p.Hitable{&sphere, &floor}}
 )
@@ -44,33 +45,34 @@ func color(r *p.Ray, h p.Hitable) p.Vector {
 	// make unit vector so y is between -1.0 and 1.0
 	unitDirection := r.Direction.Normalize()
 
+	return gradient(&unitDirection)
+}
+
+func gradient(v *p.Vector) p.Vector {
 	// scale t to be between 0.0 and 1.0
-	t := 0.5 * (unitDirection.Y + 1.0)
+	t := 0.5 * (v.Y + 1.0)
 
 	// linear blend: blended_value = (1 - t) * white + t * blue
 	return white.MultiplyScalar(1.0 - t).Add(blue.MultiplyScalar(t))
 }
 
 func main() {
-	// size of image x and y
 
 	f, err := os.Create("out.ppm")
-
 	defer f.Close()
-
 	check(err, "Error opening file: %v\n")
 
 	// http://netpbm.sourceforge.net/doc/ppm.html
 	_, err = fmt.Fprintf(f, "P3\n%d %d\n255\n", nx, ny)
-
 	check(err, "Error writting to file: %v\n")
 
 	// writes each pixel with r/g/b values
 	// from top left to bottom right
 	for j := ny - 1; j >= 0; j-- {
 		for i := 0; i < nx; i++ {
-			rgb := p.Vector{0, 0, 0}
+			rgb := p.Vector{}
 
+			// sample rays for anti-aliasing
 			for s := 0; s < ns; s++ {
 				u := (float64(i) + rand.Float64()) / float64(nx)
 				v := (float64(j) + rand.Float64()) / float64(ny)
@@ -80,6 +82,7 @@ func main() {
 				rgb = rgb.Add(color)
 			}
 
+			// average
 			rgb = rgb.DivideScalar(float64(ns))
 
 			// get intensity of colors
@@ -88,7 +91,6 @@ func main() {
 			ib := int(c * rgb.Z)
 
 			_, err = fmt.Fprintf(f, "%d %d %d\n", ir, ig, ib)
-
 			check(err, "Error writing to file: %v\n")
 		}
 	}
